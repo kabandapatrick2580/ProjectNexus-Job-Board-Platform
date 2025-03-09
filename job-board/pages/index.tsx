e7b781cc294ecc5d392504971eca4206835c0166
kabanda.patrick2580 <sendsome2019@gmail.com>
2025-03-09 23:00:23 +0200

    Readme.md

---
+++ job-board/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div className="home-container">
+            <div className="content">
+                <h1>Land your first dream job 🚀🌟</h1>
+                <p>Start your journey with us today!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
